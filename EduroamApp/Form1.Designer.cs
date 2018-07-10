6f62478210a49cc26a1ccc7f356f95d1e48850cc
Eric <eric.veliyulin@uninett.no>
2018-07-10 16:15:02 +0200

    Added all methods from prototype project, created simple UI.

--- EduroamApp/Form1.Designer.cs
+++ EduroamApp/Form1.Designer.cs
@@ -28,20 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSelectProfile = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtProfilePath = new System.Windows.Forms.TextBox();
+            this.txtCertPwd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnSelectProfile
+            // 
+            this.btnSelectProfile.Location = new System.Drawing.Point(276, 38);
+            this.btnSelectProfile.Name = "btnSelectProfile";
+            this.btnSelectProfile.Size = new System.Drawing.Size(30, 23);
+            this.btnSelectProfile.TabIndex = 0;
+            this.btnSelectProfile.Text = "...";
+            this.btnSelectProfile.UseVisualStyleBackColor = true;
+            this.btnSelectProfile.Click += new System.EventHandler(this.btnSelectProfile_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(340, 12);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(356, 249);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select network profile (XML):";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(113, 124);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(88, 45);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect to Eduroam";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtProfilePath
+            // 
+            this.txtProfilePath.Location = new System.Drawing.Point(161, 40);
+            this.txtProfilePath.Name = "txtProfilePath";
+            this.txtProfilePath.Size = new System.Drawing.Size(109, 20);
+            this.txtProfilePath.TabIndex = 5;
+            // 
+            // txtCertPwd
+            // 
+            this.txtCertPwd.Location = new System.Drawing.Point(161, 70);
+            this.txtCertPwd.Name = "txtCertPwd";
+            this.txtCertPwd.PasswordChar = '*';
+            this.txtCertPwd.Size = new System.Drawing.Size(109, 20);
+            this.txtCertPwd.TabIndex = 7;
+            this.txtCertPwd.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter certificate password:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(708, 273);
+            this.Controls.Add(this.txtCertPwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProfilePath);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnSelectProfile);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelectProfile;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtProfilePath;
+        private System.Windows.Forms.TextBox txtCertPwd;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- EduroamApp/Form1.cs
+++ EduroamApp/Form1.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ManagedNativeWifi;
+using System.Net;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security;
 
 namespace EduroamApp
 {
@@ -16,5 +23,173 @@ namespace EduroamApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelectProfile_Click(object sender, EventArgs e)
+        {
+            string rawXmlPath = GetProfileXml();
+            txtProfilePath.Text = rawXmlPath;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            AvailableNetworkPack network = SetChosenNetwork();
+            string xmlPath = txtProfilePath.Text;
+            // creates a new network profile
+            txtOutput.Text += (CreateNewProfile(network, xmlPath) ? "New profile successfully created." : "Creation of new profile failed.");
+
+            // downloads and installs client certificate and CA
+            string certPassword = txtCertPwd.Text;
+            InstallCertificate(certPassword);
+                        
+            // connects to eduroam
+
+        }
+
+
+
+
+        // -------------------------------------------- FUNCIONS ------------------------------------------------------------
+
+        /// <summary>
+        /// Sets Eduroam as the chosen network to connect to. Exits the application if Eduroam is not available.
+        /// </summary>
+        public static AvailableNetworkPack SetChosenNetwork()
+        {
+            // gets all available networks and stores them in a list
+            List<AvailableNetworkPack> networks = NativeWifi.EnumerateAvailableNetworks().ToList();
+            
+            // sets eduroam as the chosen network,
+            // prefers a network with an existing profile
+            foreach (AvailableNetworkPack network in networks)
+            {
+                if (network.Ssid.ToString() == "eduroam")
+                {
+                    if (network.ProfileName != "")
+                    {
+                        return network;
+                    }
+                    return network;
+                }
+            }
+
+            // if no networks called "eduroam" are found, return nothing
+            return null;
+        }
+
+        /// <summary>
+        /// Lets user select an XML file containing a wireless network profile.
+        /// </summary>
+        /// <returns>Path of profile xml.</returns>
+        public static string GetProfileXml()
+        {
+            string xmlPath = "not found";
+
+            OpenFileDialog openXmlDialog = new OpenFileDialog();
+
+            openXmlDialog.InitialDirectory = @"C:\Users\lwerivel18\Documents\Wireless_profiles"; // sets the initial directory of the open file dialog
+            openXmlDialog.Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*"; // sets filter for file types that appear in open file dialog
+            openXmlDialog.FilterIndex = 0;
+            openXmlDialog.RestoreDirectory = true;
+
+            if (openXmlDialog.ShowDialog() == DialogResult.OK)
+            {
+                xmlPath = openXmlDialog.FileName;
+            }
+
+            string xmlFile = File.ReadAllText(xmlPath);
+            return xmlPath;
+        }
+
+        /// <summary>
+        /// Creates new network profile according to selected network and profile XML.
+        /// </summary>
+        /// <param name="networkPack">Info about selected network.</param>
+        /// <param name="profileXml">Path of selected profile XML.</param>
+        /// <returns>True if succeeded, false if failed.</returns>
+        public static bool CreateNewProfile(AvailableNetworkPack networkPack, string profileXml)
+        {                      
+            // sets the profile type to be Per User (value = 2)
+            // if set to Per User, the security type parameter is not required
+            ProfileType newProfileType = ProfileType.PerUser;
+
+            // gets the content of the XML file
+            string xmlContent = File.ReadAllText(profileXml);
+
+            // security type not required
+            string newSecurityType = null;
+
+            // overwrites if profile already exists
+            bool overwrite = true;
+
+            return NativeWifi.SetProfile(networkPack.Interface.Id, newProfileType, xmlContent, newSecurityType, overwrite);
+        }
+
+        /// <summary>
+        /// Downloads and installs a client certificate as well as a Client Authority certificate.
+        /// </summary>
+        /// <param name="password">The certificate's password.</param>
+        public static void InstallCertificate(string password)
+        {
+            // checks if valid certificate already exists
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            X509Certificate2Collection certs = store.Certificates.Find(X509FindType.FindBySubjectName, "eduroam_certificate.p12", true);
+            if (certs.Count < 1)
+            {
+                // creates directory to download certificate file to
+                string path = $@"{ Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) }\MyCertificates";
+                Directory.CreateDirectory(path);
+                // sets the file name
+                string file = "eduroam_certificate.p12";
+                // downloads certificate from server
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile("https://nofile.io/g/HeaP3RqE5P0HVgrmGSzzA2jHWeLQbfw2a40T0vXwWKGEt38oLZ3pUEGzrPYegha3/ericv%40fyrkat.no.p12", $@"{path}\{file}");
+                    Console.WriteLine($@"Certificate downloaded to {path}\{file}.");
+                }
+
+                // installs certficate to personal certificate store
+                X509Certificate2 certificate = new X509Certificate2($@"{path}\{file}", password, X509KeyStorageFlags.PersistKeySet); // include PersistKeySet flag so certificate is valid after reboot
+                store.Add(certificate);
+
+                //Console.WriteLine("Certificate added to: " + store.Name);
+            }
+            // closes the certificate store
+            store.Close();
+
+            // adds certificate authority to trusted root certificate authority store
+            X509Store caStore = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            X509Certificate2 ca = new X509Certificate2(@"C:\Users\lwerivel18\Documents\Certificates\Fyrkat+Root+CA.crt");
+            caStore.Open(OpenFlags.ReadWrite);
+            caStore.Add(ca);
+            store.Close();
+            //Console.WriteLine("Certificate Authority added to: " + caStore.Name);
+        }
+
+
+        /// <summary>
+		/// Connects to the chosen wireless LAN
+		/// </summary>
+		/// <returns>True if successfully connected. False if not.</returns>
+		public static async Task<bool> ConnectAsync(AvailableNetworkPack networkParam)
+        {
+            AvailableNetworkPack chosenWifi = networkParam;
+
+            if (chosenWifi == null)
+                return false;
+
+            return await NativeWifi.ConnectNetworkAsync(
+                interfaceId: chosenWifi.Interface.Id,
+                profileName: chosenWifi.ProfileName,
+                bssType: chosenWifi.BssType,
+                timeout: TimeSpan.FromSeconds(100));
+        }
+
+        
     }
 }
