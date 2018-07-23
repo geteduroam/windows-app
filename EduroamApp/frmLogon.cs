59d8ba1355662eaf2db2a8198969186e31df71ac
Eric <eric.veliyulin@uninett.no>
2018-07-23 15:47:52 +0200

    Now checks if config file has valid certificate(s), if not prompts user for login with username and password.

---
+++ EduroamApp/frmLogon.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ManagedNativeWifi;
+
+namespace EduroamApp
+{
+    public partial class frmLogon : Form
+    {
+        // network pack from previous form
+        AvailableNetworkPack network;
+
+        public frmLogon()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // gets username and password from UI
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // gets network pack info from previous form
+            string ssid = network.Ssid.ToString();
+            Guid interfaceId = network.Interface.Id;
+
+            // generates user data xml file
+            string userDataXml = UserDataXml.CreateUserDataXml(username, password);
+
+            // sets user data
+            SetUserData(interfaceId, ssid, userDataXml);
+
+            // connects to eduroam                
+            var connectResult = Task.Run(() => ConnectAsync(network)).Result;
+            lblStatus.Text = (connectResult ? "Connection success." : "Connection failed.");
+        }
+
+        public void GetEduroamInstance(AvailableNetworkPack existingNetwork)
+        {
+            network = existingNetwork;
+        }
+
+        /// <summary>
+        /// Sets a profile's user data for login with username + password.
+        /// </summary>
+        /// <param name="networkID">Interface ID of selected network.</param>
+        /// <param name="profileName">Name of associated wireless profile.</param>
+        /// <param name="userDataXml">User data XML converted to string.</param>
+        /// <returns>True if succeeded, false if failed.</returns>
+        public static bool SetUserData(Guid networkID, string profileName, string userDataXml)
+        {
+            // sets the profile user type to "WLAN_SET_EAPHOST_DATA_ALL_USERS"
+            uint profileUserType = 0x00000001;
+
+            return NativeWifi.SetProfileUserData(networkID, profileName, profileUserType, userDataXml);
+        }
+
+        /// <summary>
+        /// Connects to the chosen wireless LAN.
+        /// </summary>
+        /// <returns>True if successfully connected. False if not.</returns>
+        public static async Task<bool> ConnectAsync(AvailableNetworkPack chosenWifi)
+        {
+            if (chosenWifi == null)
+                return false;
+
+            return await NativeWifi.ConnectNetworkAsync(
+                interfaceId: chosenWifi.Interface.Id,
+                profileName: chosenWifi.ProfileName,
+                bssType: chosenWifi.BssType,
+                timeout: TimeSpan.FromSeconds(10));
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
