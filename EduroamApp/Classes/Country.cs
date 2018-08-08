aae9b8410cd76d5bdc7d6c7bf10df4effb84b6ec
Eric <eric.veliyulin@uninett.no>
2018-08-08 12:09:27 +0200

    Restructured solution hierarchy (more folders!) Now displays country names instead of country codes when loading from CAT.

---
+++ EduroamApp/Classes/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduroamApp
+{
+    class Country
+    {
+        // Properties
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+
+        // Constructor
+        public Country(string countryCode, string countryName)
+        {
+            CountryCode = countryCode;
+            CountryName = countryName;
+        }
+    }
+}
