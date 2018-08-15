e03727e8c278a7e5511cbd4539c67ccd07e5742f
Eric <eric.veliyulin@uninett.no>
2018-08-15 16:30:02 +0200

    Gets json file with token.

--- EduroamApp/Forms/frmParent.cs
+++ EduroamApp/Forms/frmParent.cs
@@ -95,8 +95,7 @@ namespace EduroamApp
 					eapType = frmDownload.ConnectWithDownload();
 					if (eapType == 13) LoadFrm6();
 					else if (eapType == 25 || eapType == 21) LoadFrm5();
-					else if (eapType == 2) MessageBox.Show("Complete Feide authentication in your bowser.",
-															"Feide authentication", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					else if (eapType == 2) ;
 					else MessageBox.Show("Couldn't connect to eduroam. \nYour institution does not have a valid configuration.",
 										 "Configuration not valid", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
 					break;
