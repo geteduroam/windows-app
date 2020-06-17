c3d21353e682bdb1303d11b93e25df950cac413c
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-06-17 07:43:35 -0700

    Add exceptions.cs

---
+++ EduroamApp/Classes/Exceptions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduroamApp
+{
+	class EduroamAppUserError : Exception
+	{
+		public string UserFacingMessage { get; }
+
+		public EduroamAppUserError(string message, string userFacingMessage) : base(message)
+		{
+			UserFacingMessage = userFacingMessage;
+		}
+	}
+}
