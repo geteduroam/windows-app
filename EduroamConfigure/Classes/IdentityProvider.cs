fefeb5c9771dc9e113cba92333c156c19159482d
Simon Oliver Tveit <simtve@uninett.no>
2020-06-17 15:38:38 +0200

    add classes to EduroamConfigure

---
+++ EduroamConfigure/Classes/IdentityProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace EduroamConfigure
+{
+    /// <summary>
+    /// Stores geographical coordinates.
+    /// </summary>
+    public class Geo
+    {
+        // Properties
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+
+    // Stores information found in IdentityProvider json. 
+    public class IdentityProvider
+    {
+        public int EntityID { get; set; }
+        public string Country { get; set; }
+        public int Icon { get; set; }
+        public string Title { get; set; }
+        public List<Geo> Geo { get; set; }
+        public int Id { get; set; }
+    }
+}
