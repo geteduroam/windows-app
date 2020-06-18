e2de9fa1b33140706082de8ae64cdad350813062
Simon Oliver Tveit <simtve@uninett.no>
2020-06-18 14:49:25 +0200

    move identity provider download and parsing to new classes

---
+++ EduroamApp/Classes/IdProviderDownloader.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace EduroamApp
+{
+    class IdProviderDownloader
+    {
+        /// <summary>
+        /// Fetches a list of all eduroam institutions from https://cat.eduroam.org.
+        /// </summary>
+        /// <exception cref="EduroamAppUserError">description</exception>
+        public static List<IdentityProvider> GetAllIdProviders()
+        {
+            // url for json containing all identity providers/institutions
+            const string allIdentityProvidersUrl = "https://cat.eduroam.org/user/API.php?action=listAllIdentityProviders&lang=en";
+            try
+            {
+                string idProviderJson = GetStringFromUrl(allIdentityProvidersUrl);
+                List<IdentityProvider> identityProviders = JsonConvert.DeserializeObject<List<IdentityProvider>>(idProviderJson);
+
+                if (identityProviders.Count <= 0)
+                {
+                    throw new EduroamAppUserError("", "Institutions couldn't be read from JSON file.");
+                }
+                return identityProviders;
+            }
+            catch (WebException ex)
+            {
+                throw new EduroamAppUserError("", GetWebExceptionString(ex));
+            }
+
+        }
+        /// <summary>
+        /// Gets all profiles associated with a identity provider ID.
+        /// </summary>
+        /// <returns>identity provider profile object containing all profiles for given provider</returns>
+        /// <exception cref="EduroamAppUserError">description</exception>
+        public static IdentityProviderProfile GetIdentityProviderProfiles(int idProviderId)
+        {
+
+            // adds institution id to url
+            string profilesUrl = $"https://cat.eduroam.org/user/API.php?action=listProfiles&id={idProviderId}&lang=en";
+
+            try
+            {
+                // downloads json file as string
+                string profilesJson = GetStringFromUrl(profilesUrl);
+                // gets identity provider profile from json
+                IdentityProviderProfile idProviderProfiles = JsonConvert.DeserializeObject<IdentityProviderProfile>(profilesJson);
+                return idProviderProfiles;
+            }
+            catch (WebException ex)
+            {
+                throw new EduroamAppUserError("", GetWebExceptionString(ex));
+            }
+            catch (JsonReaderException ex)
+            {
+                throw new EduroamAppUserError("", GetJsonExceptionString(ex));
+            }
+        }
+
+        /// <summary>
+        /// Gets profile attributes for a given profile ID.
+        /// </summary>
+        /// <returns>Profile Attributes</returns>
+        public static IdProviderProfileAttributes GetProfileAttributes(string profileId)
+        {
+            // download attribute in json format
+            string profileAttributeUrl = $"https://cat.eduroam.org/user/API.php?action=profileAttributes&id={profileId}&lang=en";
+            string profileAttributeJson = GetStringFromUrl(profileAttributeUrl);
+
+            // deserialize json to get profilattributes
+            IdProviderProfileAttributes profileAttributes;
+            profileAttributes = JsonConvert.DeserializeObject<IdProviderProfileAttributes>(profileAttributeJson);
+
+            return profileAttributes;
+        }
+
+
+
+        /// <summary>
+        /// Gets download link for EAP config from json and downloads it.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="EduroamAppUserError">description</exception>
+        public static string GetEapConfigString(string profileId)
+        {
+            // adds profile ID to url containing json file, which in turn contains url to EAP config file download
+            string generateEapUrl = $"https://cat.eduroam.org/user/API.php?action=generateInstaller&id=eap-config&lang=en&profile={profileId}";
+
+            // contains json with eap config file download link
+            GenerateEapConfig eapConfigInstance;
+            try
+            {
+                // downloads json as string
+                string generateEapJson = GetStringFromUrl(generateEapUrl);
+                // converts json to GenerateEapConfig object
+                eapConfigInstance = JsonConvert.DeserializeObject<GenerateEapConfig>(generateEapJson);
+            }
+            catch (WebException ex)
+            {
+                throw new EduroamAppUserError("", GetWebExceptionString(ex));
+            }
+            catch (JsonReaderException ex)
+            {
+                throw new EduroamAppUserError("", GetJsonExceptionString(ex));
+            }
+
+            // gets url to EAP config file download from GenerateEapConfig object
+            string eapConfigUrl = $"https://cat.eduroam.org/user/{eapConfigInstance.Data.Link}";
+
+            // downloads and returns eap config file as string
+            try
+            {
+                return GetStringFromUrl(eapConfigUrl);
+            }
+            catch (WebException ex)
+            {
+                throw new EduroamAppUserError("", GetWebExceptionString(ex));
+            }
+        }
+
+        /// <summary>
+        /// Gets a json file as string from url.
+        /// </summary>
+        /// <param name="url">Url containing json file.</param>
+        /// <returns>Json string.</returns>
+        private static string GetStringFromUrl(string url)
+        {
+            // downloads json file from url as string
+            using (var client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                string jsonString = client.DownloadString(url);
+                return jsonString;
+            }
+        }
+
+
+        /// <summary>
+        /// Produces error string for web exceptions that occur if user loses an existing connection.
+        /// </summary>
+        /// <param name="ex">WebException.</param>
+        /// <returns>String containing error message meant for user to read.</returns>
+        private static string GetWebExceptionString(WebException ex)
+        {
+            string error =   "Couldn't connect to the server.\n\n"
+                    + "Make sure that you are connected to the internet, then try again.\n" +
+                    "Exception: " + ex.Message;
+            return error;
+        }
+
+
+        /// <summary>
+        /// Produces error string for exceptions related to deserializing JSON files and corrupted XML files.
+        /// </summary>
+        /// <param name="ex">Exception.</param>
+        /// <returns>String containing error message meant for user to read.</returns>
+        private static string GetJsonExceptionString(Exception ex)
+        {
+            string error = "The selected institution or profile is not supported. " +
+                            "Please select a different institution or profile.\n"
+                            + "Exception: " + ex.Message;
+            return error;
+        }
+
+    }
+       
+}
---
+++ EduroamApp/Classes/IdProviderParser.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Device.Location;
+using System.Globalization;
+
+namespace EduroamApp
+{
+    class IdProviderParser
+    {
+        /// <summary>
+        /// Finds the identity provider that is closest to the given coordinates
+        /// </summary>
+        /// <param name="instList">List of all institutions/identity providrs.</param>
+        /// <returns>The identity provider closest to coordinates.</returns>
+        /// <exception cref="EduroamAppUserError">description</exception>
+        public static IdentityProvider GetClosestIdProvider(List<IdentityProvider> instList, GeoCoordinate userCoord)
+        {
+            if (userCoord.IsUnknown)
+            {
+                throw new EduroamAppUserError("", "Found no coordinates for machine");
+
+            }
+            // institution's coordinates
+            var instCoord = new GeoCoordinate();
+            // closest institution
+            var closestInst = new IdentityProvider();
+            // shortest distance
+            double shortestDistance = double.MaxValue;
+
+            // loops through all institutions' coordinates and compares them with current shortest distance
+            foreach (IdentityProvider inst in instList)
+            {
+                if (inst.Geo != null) // excludes if geo property not set
+                {
+                    // gets lat and long
+                    instCoord.Latitude = inst.Geo.First().Lat;
+                    instCoord.Longitude = inst.Geo.First().Lon;
+                    // gets current distance
+                    double currentDistance = userCoord.GetDistanceTo(instCoord);
+                    // compares with shortest distance
+                    if (currentDistance < shortestDistance)
+                    {
+                        // sets the current distance as the shortest dstance
+                        shortestDistance = currentDistance;
+                        closestInst = inst;
+                    }
+                }
+            }
+            return closestInst;
+        }
+
+        /// <summary>
+        /// Compares institution coordinates with local machines coordinates and gets the country code from the closest institution.
+        /// If no closest insitution is found, region and language settings for the machine is used instead
+        /// </summary>
+        /// <param name="instList">List of all institutions.</param>
+        /// <param name="userCoord"> Coordinates to compare with institution coordinates.</param>
+        /// <returns>Country Code of closest institution.</returns>
+        public static string GetClosestCountryCode(List<IdentityProvider> instList, GeoCoordinate userCoord)
+        {
+
+            //Reads country code from local machine if no coordinates found
+            try
+            {
+                return GetClosestIdProvider(instList, userCoord).Country;
+            }
+            catch (EduroamAppUserError ex)
+            {
+
+            }
+
+            // gets country as set in Settings
+            // https://stackoverflow.com/questions/8879259/get-current-location-as-specified-in-region-and-language-in-c-sharp
+            var regKeyGeoId = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Control Panel\International\Geo");
+            var geoID = (string)regKeyGeoId.GetValue("Nation");
+            var allRegions = CultureInfo.GetCultures(CultureTypes.SpecificCultures).Select(x => new RegionInfo(x.ToString()));
+            var regionInfo = allRegions.FirstOrDefault(r => r.GeoId == Int32.Parse(geoID));
+            return regionInfo.TwoLetterISORegionName;
+        }
+
+        /// <summary>
+        /// Turns list of identity providers into a list containing all countries found in the list
+        /// </summary>
+        /// <param name="instList">List of all institutions/identity providrs.</param>
+        /// <returns>List of all countries found.</returns>
+        public static List<Country> GetCountries(List<IdentityProvider> instList)
+        {
+            List<Country> countries = new List<Country>();
+            List<string> distinctCountryCodes = instList.Select(provider => provider.Country)
+                                                                 .Distinct().ToList();
+            // converts all country codes to country names and puts them in a list
+            foreach (string countryCode in distinctCountryCodes)
+            {
+                string countryName;
+                try
+                {
+                    var countryInfo = new RegionInfo(countryCode);
+                    countryName = countryInfo.DisplayName;
+                }
+                // if "country" from json file does not have associated RegionInfo, set country code as country name
+                catch (ArgumentException)
+                {
+                    countryName = countryCode;
+                }
+
+                countries.Add(new Country(countryCode, countryName));
+            }
+            return countries;
+        }
+        
+
+        /// <summary>
+        /// Gets redirect link from profile's attributes
+        /// </summary>
+        /// <returns>Redirect link, if exists.</returns>
+        public static string getRedirect(IdProviderProfileAttributes attributes)
+        {
+            // checks profile attributes for a redirect link
+            var redirect = "";
+            foreach (var attribute in attributes.Data.Devices)
+            {
+                if (attribute.Redirect != "0")
+                {
+                    redirect = attribute.Redirect;
+                }
+            }
+            return redirect;
+        }
+    }
+}
