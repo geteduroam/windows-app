4dd2a8ab00e6476f1f3e69855f76921e48026b11
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-07-31 03:58:27 -0700

    Make the user defined exceptions serializable

---
+++ EduroamConfigure/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "This exception is intended to limit how you can use it", Scope = "type", Target = "~T:EduroamConfigure.EduroamAppUserError")]
