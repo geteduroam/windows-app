3ae053c31e934c6b374501d4ae9bca4ee8c70862
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-07-31 03:58:27 -0700

    Make the user defined exceptions serializable

--- EduroamConfigure/Exceptions.cs
+++ EduroamConfigure/Exceptions.cs
@@ -16,31 +16,41 @@ namespace EduroamConfigure
 #endif
 		}
 	}
+
+	[Serializable]
 	public class ApiException : Exception
 	{
 		private const string DefaultMessage = "Error with Api";
 
 		public ApiException() : base(DefaultMessage) { }
 		public ApiException(string message) : base(message) { }
-		public ApiException(string message, System.Exception innerException) : base(message, innerException) { }
+		public ApiException(string message, Exception innerException) : base(message, innerException) { }
+		protected ApiException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
 	}
 
+	[Serializable]
 	public class ApiUnreachableException : ApiException
 	{
 		private const string DefaultMessage = "Api could not be reached";
 
 		public ApiUnreachableException() : base(DefaultMessage) { }
 		public ApiUnreachableException(string message) : base(message) { }
-		public ApiUnreachableException(string message, System.Exception innerException) : base(message, innerException) { }
+		public ApiUnreachableException(string message, Exception innerException) : base(message, innerException) { }
+
+		protected ApiUnreachableException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 	}
 
+	[Serializable]
 	public class ApiParsingException : Exception
 	{
 		private const string DefaultMessage = "Api response could not be parsed";
 
 		public ApiParsingException() : base(DefaultMessage) { }
 		public ApiParsingException(string message) : base(message) { }
-		public ApiParsingException(string message, System.Exception innerException) : base(message, innerException) { }
+		public ApiParsingException(string message, Exception innerException) : base(message, innerException) { }
+
+		protected ApiParsingException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 	}
 }
 
