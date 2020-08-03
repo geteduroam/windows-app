cf21eb89b9741bf8b79559e927efce214557e45f
simtve <simtve@uninett.no>
2020-08-03 15:41:47 +0200

    easy to add separators and grids

--- WpfApp/Menu/CertificateOverview.xaml.cs
+++ WpfApp/Menu/CertificateOverview.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using EduroamConfigure;
+using WpfApp.Classes;
 
 namespace WpfApp.Menu
 {
@@ -34,6 +35,34 @@ namespace WpfApp.Menu
 		private void Load()
 		{
 
+			AddCertGrid("Cert1");
+			AddSeparator();
+			AddCertGrid("Cert2");
+			AddSeparator();
+			AddCertGrid("Cert3");
+		}
+
+		private void AddCertGrid(string text)
+		{
+			CertificateGrid grid = new CertificateGrid();
+			grid.Text = text;
+			grid.Margin = new Thickness(5, 5, 5, 5);
+			AddToStack(grid);
+		}
+
+		private void AddSeparator()
+		{
+			Separator sep = new Separator();
+			sep.Height = 1;
+			sep.BorderThickness = new Thickness(1, 1, 1, 1);
+			sep.BorderBrush = Brushes.LightGray;
+			sep.Margin = new Thickness(5, 0, 5, 0);
+			AddToStack(sep);
+		}
+
+		private void AddToStack(Control uc)
+		{
+			stpCerts.Children.Add(uc);
 		}
 	}
 }
