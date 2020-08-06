88fe0e6f9d9ce93014427daff8fda450bf9856eb
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-08-06 06:06:14 -0700

    Update assembly info

--- WpfApp/Properties/AssemblyInfo.cs
+++ WpfApp/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("geteduroam for Windows")]
+[assembly: AssemblyDescription("eduroam configuration utility")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApp")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("Uninett AS")]
+[assembly: AssemblyProduct("geteduroam")]
+[assembly: AssemblyCopyright("Copyright © 2018-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
