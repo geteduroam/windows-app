cfa5b7b72c4688f0f1944b926a38e4c91edc3458
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-08-10 02:39:17 -0700

    Make the isWin64 check into isX86, in preparation for possible ARM32 support

--- EduroamConfigure/UserDataXml.cs
+++ EduroamConfigure/UserDataXml.cs
@@ -198,17 +198,17 @@ namespace EduroamConfigure
 
         public static bool IsSupported(EapType eapType, InnerAuthType innerAuthType)
         {
-            var isWin64 = IntPtr.Size >= 8;
-            Debug.Assert(isWin64);
+            var isX86 = System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture == System.Runtime.InteropServices.Architecture.X86;
+            Debug.Assert(!isX86);
             //bool at_least_win10 = System.Environment.OSVersion.Version.Major >= 10; // TODO: make this work, requires some application manifest
             var at_least_win10 = true;
             return (eapType, innerAuthType) switch
             {
                 (EapType.TLS, _) => true,
                 (EapType.PEAP, InnerAuthType.EAP_MSCHAPv2) => true,
-                (EapType.TTLS, InnerAuthType.PAP) => isWin64,
-                (EapType.TTLS, InnerAuthType.MSCHAP) => isWin64, // not tested, but matches schema
-                (EapType.TTLS, InnerAuthType.MSCHAPv2) => isWin64,
+                (EapType.TTLS, InnerAuthType.PAP) => !isX86,
+                (EapType.TTLS, InnerAuthType.MSCHAP) => !isX86, // not tested, but matches schema
+                (EapType.TTLS, InnerAuthType.MSCHAPv2) => !isX86,
                 //(EapType.TTLS, InnerAuthType.EAP_MSCHAPv2) => at_least_win10 && geWin64, // TODO: xml matches the schema, but win32 throws an error.
                 //(EapType.TTLS, InnerAuthType.EAP_PEAP_MSCHAPv2) => at_least_win10 && geWin64, // TODO: xml matches the schema, but win32 throws an error.
                 _ => false,
