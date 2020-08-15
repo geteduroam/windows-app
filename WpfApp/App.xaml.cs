ad5b6b5f1eba1848a40f305e3b0e1dc10a3cc4d9
Jørn Åne de Jong <jorn.dejong@uninett.no>
2020-08-15 14:06:54 +0200

    Show uninstall confirmation dialog

--- WpfApp/App.xaml.cs
+++ WpfApp/App.xaml.cs
@@ -224,13 +224,28 @@ namespace WpfApp
 			Installer.ExitAndUninstallSelf(
 				success =>
 				{
-					// we cannot show a message box on success,
+					// we cannot show a normal message box on success,
 					// since we've dispatched a job to delete the running binary at this point
-					if (!success) MessageBox.Show(
+					// but we can spawn a PowerShell that will show the success message
+					if (success) {
+						var extinguishMe = new ProcessStartInfo
+						{
+							FileName = "mshta",
+							Arguments = "vbscript:Execute(\"msgbox \"\"The application and its configuration have been uninstalled\"\", 0, \"\"Uninstall geteduroam\"\":close\")",
+							WindowStyle = ProcessWindowStyle.Normal, // Shows a console in the taskbar, but it's hidden
+							CreateNoWindow = true,
+							WorkingDirectory = "C:\\"
+						};
+						Process.Start(extinguishMe);
+					}
+					else
+					{
+						MessageBox.Show(
 						"geteduroam is not yet uninstalled! The uninstallation was aborted.",
 						caption: "Uninstall geteduroam",
 						MessageBoxButton.OK,
 						MessageBoxImage.Error);
+					}
 					return shutdown(success);
 				},
 				doDeleteSelf: true);
