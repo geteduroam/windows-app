193936e4f819ca659456fd0018c3f9e2a8157888
Jørn Åne de Jong <jorn.dejong@uninett.no>
2021-05-16 21:38:47 +0200

    Remove resize function, XAML does this now

--- WpfApp/Classes/ImageFunctions.cs
+++ WpfApp/Classes/ImageFunctions.cs
@@ -1,6 +1,4 @@
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Windows.Media.Imaging;
 
@@ -37,36 +35,6 @@ namespace WpfApp
 			}
 			image.Freeze();
 			return image;
-		}
-
-		/// <summary>
-		/// Resize image to specified width and height.
-		/// </summary>
-		/// <param name="image">The image to resize.</param>
-		/// <param name="width">The width to resize to.</param>
-		/// <param name="height">The height to resize to.</param>
-		/// <returns>The resized image.</returns>
-		public static Bitmap ResizeImage(Image image, int width, int height)
-		{
-			var destRect = new Rectangle(0, 0, width, height);
-			var destImage = new Bitmap(width, height);
-
-			destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-			using (var graphics = Graphics.FromImage(destImage))
-			{
-				graphics.CompositingMode = CompositingMode.SourceCopy;
-				graphics.CompositingQuality = CompositingQuality.HighQuality;
-				graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-				graphics.SmoothingMode = SmoothingMode.HighQuality;
-				graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-				using var wrapMode = new ImageAttributes();
-				wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-				graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-			}
-
-			return destImage;
 		}
 
 		/// <summary>
