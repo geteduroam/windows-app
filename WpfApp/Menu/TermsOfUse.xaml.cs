abedcac3a8363a9876e2f2f59a4af229860b1c47
Jørn Åne de Jong <jorn.dejong@letswifi.eu>
2022-02-10 00:16:48 +0000

    Make ToU links clickable
    Replaces the ToU text field with a webbrowser component,
    and adds links, line breaks and paragraphs.

--- WpfApp/Menu/TermsOfUse.xaml.cs
+++ WpfApp/Menu/TermsOfUse.xaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text.RegularExpressions;
 using System.Windows.Controls;
 
 namespace WpfApp.Menu
@@ -19,7 +21,59 @@ namespace WpfApp.Menu
 
 		private void Load()
 		{
-			this.tbTou.Text = tou;
+			webTou.NavigateToString(GetTouHtml(tou));
+		}
+
+		private string GetTouHtml(string tou)
+		{
+			var maybeBom = tou.Substring(0, 3);
+			var bom = new string(new char[] { (char)0xEF, (char)0xBB, (char)0xBF });
+
+			if (maybeBom.Equals(bom))
+			{
+				tou = tou.Substring(3);
+			}
+
+			bool fixLinks = !Regex.IsMatch(tou, "(^|\\s)<a[\\S\t ]*>");
+			bool fixNewlines = !Regex.IsMatch(tou, "(^|\\s)<(p|br)[\\S\t ]*>");
+
+			if (fixNewlines)
+			{
+				tou = Regex.Replace(tou, "([\t ]*\r?\n){2,}", " <p>");
+				tou = Regex.Replace(tou, "([\t ]*\r?\n)", "<br>");
+			}
+
+
+			if (fixLinks)
+			{
+				// Split on space (so we can check per word if it is a link)
+				string[] touSplit = Regex.Split(tou, "[\\s ]+");
+
+				for (int i = 0; i < touSplit.Length; i++)
+				{
+					var w = touSplit[i];
+					if (w.StartsWith("www.") || w.StartsWith("http://") || w.StartsWith("https://"))
+					{
+						var link = w.StartsWith("http") ? w : "http://" + w;
+						touSplit[i] = "<a target=\"_blank\" href=\"" + link + "\">" + w + "</a>";
+					}
+				}
+
+				tou = String.Join(" ", touSplit);
+			}
+
+			return
+				"<!DOCTYPE html>" +
+				"<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">" +
+				"<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" +
+				"<style>" +
+					"body {" +
+						"margin: 2em;" +
+						"justify-content: center;" +
+						"font-family: \"Segoe UI\", \"Tahoma\", sans-serif;" +
+					"}" +
+				"</style>" +
+				tou;
 		}
 	}
 }
