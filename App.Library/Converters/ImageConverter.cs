c284a6af1698c5cd10a5523dae38277979dd3704
Juri van Tuijl <juri.vantuijl@winvision.nl>
2023-07-10 15:10:17 +0200

    app with logo

--- App.Library/Converters/ImageConverter.cs
+++ App.Library/Converters/ImageConverter.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using System.IO;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
@@ -8,24 +7,21 @@ namespace App.Library.Converters
 {
     public class ImageConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object? parameter, CultureInfo culture)
         {
             var valueAsString = value?.ToString();
+
+            if (parameter != null
+                && parameter is string)
+            {
+                valueAsString = parameter.ToString();
+            }
 
             if (value != null
                 && !string.IsNullOrEmpty(valueAsString))
             {
-                var isRelativePath = string.IsNullOrEmpty(Path.GetPathRoot(valueAsString));
-
-                if (isRelativePath)
-                {
-                    valueAsString = Path.Combine(Directory.GetCurrentDirectory(), valueAsString);
-                }
-
-                if (File.Exists(valueAsString))
-                {
-                    return new BitmapImage(new Uri(valueAsString));
-                }
+                var fileName = $@"pack://application:,,,/App.Library;component/Images/{valueAsString}";
+                return new BitmapImage(new Uri(fileName));
             }
 
             return null;
