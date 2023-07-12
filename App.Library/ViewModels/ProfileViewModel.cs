14b576bc889e2d55e5a3f00ed8e0089bd6819aaf
Juri van Tuijl <juri.vantuijl@winvision.nl>
2023-07-12 15:56:15 +0200

    added profile template

--- App.Library/ViewModels/ProfileViewModel.cs
+++ App.Library/ViewModels/ProfileViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+
+using App.Library.Command;
 
 using EduRoam.Connect;
 
@@ -6,19 +9,118 @@ namespace App.Library.ViewModels
 {
     public class ProfileViewModel : BaseViewModel
     {
+        private readonly EapConfig eapConfig;
+
         public ProfileViewModel(MainViewModel mainViewModel, EapConfig eapConfig)
             : base(mainViewModel)
         {
+            this.eapConfig = eapConfig;
+            this.NavigateWebCommand = new DelegateCommand(this.NavigateWeb, this.CanNavigateWeb);
+            this.OpenEmailCommand = new DelegateCommand(this.OpenEmail, this.CanOpenEmail);
+            this.SelectOtherInstitutionCommand = new DelegateCommand(this.OpenEmail, () => true);
+
+            //todo ExtractFlag?
+            //todo CopyToClipboard WebAddress / Phone / Email?
+        }
+
+        public DelegateCommand NavigateWebCommand { get; private set; }
+
+        public DelegateCommand OpenEmailCommand { get; private set; }
+
+        public DelegateCommand SelectOtherInstitutionCommand { get; private set; }
+
+        public string Name => this.eapConfig.InstitutionInfo.DisplayName;
+
+        public string Description => this.eapConfig.InstitutionInfo.Description;
+
+        public bool HasContactInfo
+        {
+            get
+            {
+                var hasWebAddress = !string.IsNullOrEmpty(this.eapConfig.InstitutionInfo.WebAddress);
+                var hasEmailAddress = !string.IsNullOrEmpty(this.eapConfig.InstitutionInfo.EmailAddress);
+                var hasPhone = !string.IsNullOrEmpty(this.eapConfig.InstitutionInfo.Phone);
+                return (hasWebAddress || hasEmailAddress || hasPhone);
+            }
+        }
+
+        public string WebAddress => this.eapConfig.InstitutionInfo.WebAddress;
+
+        public bool WebAddressIsValid =>
+            Uri.TryCreate(this.WebAddress, UriKind.Absolute, out var uriResult)
+            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+        public string EmailAddress => this.eapConfig.InstitutionInfo.EmailAddress;
+
+        public bool EmailIsValid => !(this.EmailAddress.Contains(" ") || !this.EmailAddress.Contains("@"));
+
+        public string Phone => this.eapConfig.InstitutionInfo.Phone;
+
+        public bool PhoneIsValid => !string.IsNullOrEmpty(this.Phone);
+
+        public string TermsOfUse => this.eapConfig.InstitutionInfo.TermsOfUse;
+
+        public bool HasTermsOfUse => !string.IsNullOrEmpty(this.TermsOfUse);
+
+        private bool CanNavigateWeb()
+        {
+            return this.WebAddressIsValid;
+        }
+
+        private void NavigateWeb()
+        {
+            Process.Start(new ProcessStartInfo(new Uri(this.WebAddress).ToString()));
+        }
+
+        private bool CanOpenEmail()
+        {
+            return this.EmailIsValid;
+        }
+
+        private void OpenEmail()
+        {
+            Process.Start(new ProcessStartInfo(new Uri("mailto:" + this.EmailAddress).ToString()));
+        }
+
+        private void SelectOtherInstitution()
+        {
+           MainViewModel.Restart();
         }
 
         protected override bool CanGoNext()
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         protected override void GoNext()
         {
-            throw new NotImplementedException();
+            //todo ShowTou was always true in old situation, What to do?
+
+            //if (pageProfileOverview.ShowTou)
+            //{
+            //    LoadPageTermsOfUse();
+            //    break;
+            //}
+            //if (ConnectToEduroam.EnumerateCAInstallers(eapConfig)
+            //.Any(installer => installer.IsInstalledByUs || !installer.IsInstalled))
+            //{
+            //    LoadPageCertificateOverview();
+            //    break;
+            //}
+
+            //LoadPageLogin();
+        }
+
+        /// <summary>
+        /// Loads the logo form the curent eapconfig if it exists. Else display Eduroam logo.
+        /// </summary>
+        private void LoadProviderLogo()
+        {
+            //todo svg support imageConverter,
+            //todo seperate image not override eduram logo
+
+            //byte[] logoBytes = eapConfig.InstitutionInfo.LogoData;
+            //string logoMimeType = eapConfig.InstitutionInfo.LogoMimeType;
         }
     }
 }
