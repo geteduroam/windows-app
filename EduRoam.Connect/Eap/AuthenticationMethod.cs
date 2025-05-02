aada2b54602da97c1eaf3684c39eaa25b75fb3bb
Jørn Åne de Jong <jorn.dejong@letswifi.eu>
2025-05-02 10:31:30 +0200

    Update fingerprint targeting, allow fingerprint to replace CA

--- EduRoam.Connect/Eap/AuthenticationMethod.cs
+++ EduRoam.Connect/Eap/AuthenticationMethod.cs
@@ -21,9 +21,9 @@ namespace EduRoam.Connect.Eap
         public EapConfig? EapConfig { get; } // reference to parent EapConfig
         public EapType EapType { get; }
         public InnerAuthType InnerAuthType { get; }
-        public List<string> ServerCertificateAuthorities { get; } // base64 encoded DER certificate
-        public List<string> CertificateThumbprints { get; }
-        public List<string> ServerNames { get; }
+        public ICollection<(string Type, string B64)> ServerCertificateAuthorities { get; } // type, base64 encoded DER certificate
+        public ICollection<(string ReplaceType, string HEX)> CertificateThumbprints { get; } // CA type to replace, HEX thumbprint
+        public ICollection<string> ServerNames { get; }
         public string? ClientUserName { get; } // preset inner identity, expect it to have a realm
         public string? ClientPassword { get; } // preset outer identity
         public string? ClientCertificate { get; } // base64 encoded PKCS12 certificate+privkey bundle
@@ -95,15 +95,21 @@ namespace EduRoam.Connect.Eap
         /// <remarks>The certificates must be disposed after use</remarks>
         public IEnumerable<X509Certificate2> CertificateAuthoritiesAsX509Certificate2()
         {
-            if (this.CertificateThumbprints.Count != 0) yield break;
+            var removeTypes = new HashSet<string>();
+            foreach (var thumbprint in CertificateThumbprints)
+            {
+                removeTypes.Add(thumbprint.ReplaceType);
+            }
 
             foreach (var ca in this.ServerCertificateAuthorities)
             {
+                if (!string.IsNullOrEmpty(ca.Type) && removeTypes.Contains(ca.Type)) continue;
+
                 X509Certificate2 cert;
                 try
                 {
                     // TODO: find some nice way to ensure these are disposed of properly
-                    cert = new X509Certificate2(Convert.FromBase64String(ca));
+                    cert = new X509Certificate2(Convert.FromBase64String(ca.B64));
                 }
                 catch (CryptographicException)
                 {
@@ -344,9 +350,9 @@ namespace EduRoam.Connect.Eap
         internal AuthenticationMethod(
             EapType eapType,
             InnerAuthType innerAuthType,
-            List<string> serverCertificateAuthorities,
-            List<string> serverName,
-            List<string> certificateThumbprints = null,
+            ICollection<(string,string)> serverCertificateAuthorities,
+            ICollection<string> serverName,
+            ICollection<(string,string)> certificateThumbprints = null,
             string? clientUserName = null,
             string? clientPassword = null,
             string? clientCertificate = null,
@@ -354,15 +360,29 @@ namespace EduRoam.Connect.Eap
             string? clientOuterIdentity = null,
             string? innerIdentitySuffix = null,
             bool innerIdentityHint = false
-        ) : this(null, eapType, innerAuthType, serverCertificateAuthorities, serverName, certificateThumbprints, clientUserName, clientPassword, clientCertificate, clientCertificatePassphrase, clientOuterIdentity, innerIdentitySuffix, innerIdentityHint) { }
+        ) : this(
+            null, 
+            eapType, 
+            innerAuthType, 
+            serverCertificateAuthorities, 
+            serverName, 
+            certificateThumbprints, 
+            clientUserName, 
+            clientPassword, 
+            clientCertificate, 
+            clientCertificatePassphrase, 
+            clientOuterIdentity, 
+            innerIdentitySuffix, 
+            innerIdentityHint
+        ) { }
 
         private AuthenticationMethod(
             EapConfig? eapConfig,
             EapType eapType,
             InnerAuthType innerAuthType,
-            List<string> serverCertificateAuthorities,
-            List<string> serverName,
-            List<string> certificateThumbprints = null,
+            ICollection<(string,string)> serverCertificateAuthorities,
+            ICollection<string> serverName,
+            ICollection<(string,string)> certificateThumbprints = null,
             string? clientUserName = null,
             string? clientPassword = null,
             string? clientCertificate = null,
@@ -375,9 +395,9 @@ namespace EduRoam.Connect.Eap
             this.EapConfig = eapConfig;
             this.EapType = eapType;
             this.InnerAuthType = innerAuthType;
-            this.ServerCertificateAuthorities = serverCertificateAuthorities ?? new List<string>();
-            this.CertificateThumbprints = certificateThumbprints ?? new List<string>();
-            this.ServerNames = serverName ?? new List<string>();
+            this.ServerCertificateAuthorities = serverCertificateAuthorities ?? new HashSet<(string,string)>();
+            this.ServerNames = serverName ?? new HashSet<string>();
+            this.CertificateThumbprints = certificateThumbprints ?? new HashSet<(string, string)>();
             this.ClientUserName = clientUserName;
             this.ClientPassword = clientPassword;
             this.ClientCertificate = clientCertificate;
--- EduRoam.Connect/Eap/EapConfig.cs
+++ EduRoam.Connect/Eap/EapConfig.cs
@@ -105,18 +105,16 @@ namespace EduRoam.Connect.Eap
 
                 // ServerSideCredential
 
-                var serverFingerprints = authMethodXml
-                    .Elements().First(nameIs("EAPMethod"))
-                    .Elements().Where(nameIs("VendorSpecific")).First(xElement => xElement.Attribute("vendor").Value == "1023")
-                    .Elements().FirstOrDefault(nameIs("ServerSideCredential"))?
+                // get list of fingerprints and the CAs they will replace
+                var serverFingerprints = serverSideCredentialXml?
                     .Elements().Where(nameIs("Fingerprint"))
-                    .Select(xElement => (string)xElement)
+                    .Select(xElement => ((string)xElement.Attribute("replace")?.Value, (string)xElement))
                     .ToList();
 
-                // get list of strings of CA certificates
+                // get list of strings of CA certificates and the type, which is a selector for replacement
                 var serverCAs = serverSideCredentialXml?
                     .Elements().Where(nameIs("CA")) // TODO: <CA format="X.509" encoding="base64"> is assumed, schema does not enforce this
-                    .Select(xElement => (string)xElement)
+                    .Select(xElement => ((string)xElement.Attribute("type")?.Value, (string)xElement))
                     .ToList();
 
                 // get list of strings of server IDs
@@ -149,9 +147,9 @@ namespace EduRoam.Connect.Eap
                 authMethods.Add(new AuthenticationMethod(
                     eapType,
                     innerAuthType,
-                    serverCAs ?? new List<string>(),
+                    serverCAs ?? new List<(string,string)>(),
                     serverNames ?? new List<string>(),
-                    serverFingerprints ?? new List<string>(),
+                    serverFingerprints ?? new List<(string,string)>(),
                     clientUserName,
                     clientPassword,
                     clientCert,
