c31d3dea642b9e2cf186258b6fc04f7d258cbfd2
Rick Bonkestoter <r.bonkestoter@previder.nl>
2025-12-01 16:40:15 +0100

    #90 Added custom usercontrol for password reveal (Proof of Concept)

---
+++ App.Library/UserControls/EPasswordBox.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace App.Library.UserControls
+{
+    public partial class EPasswordBox : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                nameof(Value),
+                typeof(string),
+                typeof(EPasswordBox),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnValueChanged));
+
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public bool IsPasswordRevealed
+        {
+            get => (bool)GetValue(IsPasswordRevealedProperty);
+            set => SetValue(IsPasswordRevealedProperty, value);
+        }
+
+        public static readonly DependencyProperty IsPasswordRevealedProperty =
+            DependencyProperty.Register(
+                nameof(IsPasswordRevealed),
+                typeof(bool),
+                typeof(EPasswordBox),
+                new PropertyMetadata(false, OnIsPasswordRevealedChanged));
+
+        public bool IsPasswordHidden => !IsPasswordRevealed;
+
+        public EPasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (EPasswordBox)d;
+            var newValue = e.NewValue as string ?? string.Empty;
+            if (control.IsPasswordRevealed)
+            {
+                control.TextBox.Text = newValue;
+            }
+            else
+            {
+                control.PasswordBox.Password = newValue;
+            }
+        }
+
+        private static void OnIsPasswordRevealedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (EPasswordBox)d;
+            if ((bool)e.NewValue)
+            {
+                control.TextBox.Text = control.PasswordBox.Password;
+            }
+            else
+            {
+                control.PasswordBox.Password = control.TextBox.Text;
+            }
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Value = PasswordBox.Password;
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsPasswordRevealed = !IsPasswordRevealed;
+        }
+    }
+}
